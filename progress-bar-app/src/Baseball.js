cd1bf2e0934018a70cc5d2b962fc1b5a317f05f9
HarrisonKeyser <112505168+HarrisonKeyser@users.noreply.github.com>
2024-03-07 04:31:44 +0000

    updated Baseball and removed redirect

--- progress-bar-app/src/Baseball.js
+++ progress-bar-app/src/Baseball.js
@@ -12,8 +12,9 @@ function Baseball() {
         <Link to="/" style={{ position: 'absolute', top: '20px', left: '20px' }}>
           <img src={backArrowUrl} alt="Go back to homepage" style={{ width: '30px', height: '30px', cursor: 'pointer' }} />
         </Link>
-        <h1>Baseball Statistics with Poisson Distribution</h1>
-        <p>This page will talk about predicting home runs in a baseball game using a Poisson distribution.</p>
+        <h1>Coming Soon...</h1>
+        {/* Baseball Statistics with Poisson Distribution */}
+        <p>Predicting home runs in a baseball game using a Poisson distribution.</p>
       </header>
     </div>
   );
